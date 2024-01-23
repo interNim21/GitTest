36c35e14609741715d4740f35992e6d336098696
interNim21 <thdaudgh0102@gmail.com>
2024-01-24 02:01:11 +0900

    homework

--- 3_Javascript/JS/Homework.js
+++ 3_Javascript/JS/Homework.js
@@ -1,18 +1,24 @@
 
+
 function executeFn3() {
-    const start = Number(document.getElementById("input3").value);
+    // start는 입력받은 값
+    const inputValue = Number(document.getElementById("input3").value);
     
 
     // 결과를 출력하기 위한 ul 요소를 들여와 저장함.
     const ul = document.getElementById("result3"); 
     ul.innerHTML = "";
 
-    for(let num = start; num <= 9; num++){
 
-        ul.innerHTML += `<li>${start} x ${num} = ${start * num}</li>`;
-        
+    if (inputValue < 2 || inputValue > 9) {
+        return alert("error");
+    }
+
+    
+    for(let num = 2; num <= 9; num++){
+
+        ul.innerHTML += `<li>${inputValue} x ${num} = ${inputValue * num}</li>`;
         
         
     }
-    
 }
